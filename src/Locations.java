d515b8610f6a5823b9470322edaafdd01b0b23bb
Cain <36238059+CainDev@users.noreply.github.com>
2026-08-30 18:17:01 +0100

    Add files via upload

---
+++ src/Locations.java
@@ -0,0 +1,6 @@
+import org.dreambot.api.methods.map.Area;
+
+public class Locations {
+    public static final Area REDWOOD_TREE = new Area(1566, 3497, 1575, 3478, 1);
+    public static final Area WOODCUTTING_GUILD_BANK = new Area(1589, 3480, 1593, 3475);
+}
